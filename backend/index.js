297ff197d30ba6a56cdf379510b182663a393d79
GOVIND DWIVEDI <govindofficial625@gmail.com>
2025-04-08 03:16:04 +0530

    Convert to ES Module syntax

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
--- backend/routes/volunteers.js
+++ backend/routes/volunteers.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
